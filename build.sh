7fde47035101de9f3fe3c5c8dd8797877d0a8a58
Yosri Ketata <88405393+youtarie560@users.noreply.github.com>
2025-12-01 21:24:01 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "TODO: fill in the docker build command"
+echo "Building docker image..."
+docker build -t ift6758-serving -f Dockerfile.serving .
+
+# to adjust build.sh for part 6:
+docker build -t ift6758-streamlit -f Dockerfile.streamlit .
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-echo "TODO: fill in the docker run command"
+echo "Running docker container..."
+docker run -p 8080:8080 -e API_KEY=${API_KEY} ift6758-serving
