35f8654aa84ebb79ba96438360ed59c9693c05e7
Yosri Ketata <88405393+youtarie560@users.noreply.github.com>
2025-12-06 20:30:34 -0500

    Clean up comments in run-streamlit.sh
    Removed comments about host.docker.internal usage.

--- run-streamlit.sh
+++ run-streamlit.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 echo "Running Streamlit container..."
-
-# Note: We use 'host.docker.internal' to allow this container to talk
-# to the Serving container running on localhost:8080 on your machine.
-# otherwise try host.docker.internal
-# otherwise try serving
 docker run -p 8501:8501 \
   -e BACKEND_URL="http://host.docker.internal:8080" \
   ift6758-streamlit
